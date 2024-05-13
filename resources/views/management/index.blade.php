5afc5b75b280df331597f4127a8a305e6766dd2c
Janos Stelli <stellijanos23@gmail.com>
2024-05-13 10:34:00 +0300

    create and design the data management page

---
+++ resources/views/management/index.blade.php
@@ -0,0 +1,23 @@
+
+
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <div class="list-group">
+                <a href="" class="list-group-item list-group-item-action"><i class="fa-solid fa-bars"></i> Category</a>
+                <a href="" class="list-group-item list-group-item-action"><i class="fa-solid fa-burger"></i> Menu</a>
+                <a href="" class="list-group-item list-group-item-action"><i class="fa-solid fa-chair"></i> Chair</a>
+                <a href="" class="list-group-item list-group-item-action"><i class="fa-solid fa-users-gear"></i> User</a>
+            </div>
+        </div>
+        <div class="col-md-8">
+
+        </div>
+    </div>
+</div>
+
+@endsection
