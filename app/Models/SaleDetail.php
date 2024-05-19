dfcbf3bbdc79619d99ddc312695f6b16609db961
Janos Stelli <stellijanos23@gmail.com>
2024-05-19 17:35:26 +0300

    map a relation between sales and saledetails tables

--- app/Models/SaleDetail.php
+++ app/Models/SaleDetail.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SaleDetail extends Model
 {
     use HasFactory;
+
+    public function sale(): BelongsTo {
+        return $this->belongsTo(Sale::class);
+    }
 }
