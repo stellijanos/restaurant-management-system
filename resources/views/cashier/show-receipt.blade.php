0316b0c9a4fdf11418401cb760cf28185ee093d4
Janos Stelli <stellijanos23@gmail.com>
2024-05-20 17:29:02 +0300

    style a receipt by using css

--- resources/views/cashier/show-receipt.blade.php
+++ resources/views/cashier/show-receipt.blade.php
@@ -6,6 +6,75 @@
     <title>Restaurant App | Receipt - SaleID: {{$sale->id}}</title>
 </head>
 <body>
-    
+    <div id="wrapper">
+        <div id="recept-header">
+            <h3 id="restaurant-name">Restaurant App - Janos</h3>
+            <p>Address: Str. Restaurants Nr. 5</p>
+            <p>Cluj-Napoca, Cluj</p>
+            <p>Tel: +407XXXXXXX6</p>
+            <p>Reference Receipt: <strong>{{$sale->id}}</strong></p>
+        </div>
+        <div id="recept-body">
+            <table class="tb-sale-detail">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Menu</th>
+                        <th>Qty</th>
+                        <th>Price</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($saleDetails as $saleDetail)
+                        <tr>
+                            <th width="30">{{$saleDetail->menu_id}}</th>
+                            <th width="180">{{$saleDetail->menu_name}}</th>
+                            <th width="50">{{$saleDetail->quantity}}</th>
+                            <th width="55">{{$saleDetail->menu_price}}</th>
+                            <th width="65">{{$saleDetail->menu_price * $saleDetail->quantity}}</th>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <table class="tb-sale-total">
+                <tbody>
+                    <tr>
+                        <td>Total Quantity</td>
+                        <td>{{$saleDetails->count()}}</td>
+                        <td>Total</td>
+                        <td>${{number_format($sale->total_price, 2)}}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Payment Type</td>
+                        <td colspan="2">{{$sale->payment_type}}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Paid Amount</td>
+                        <td colspan="2">${{number_format($sale->total_recieved, 2)}}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">Change</td>
+                        <td colspan="2">${{number_format($sale->change, 2)}}</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+        </div>
+        <div id="recept-footer">
+            <p>Thank you for eating by us!</p>
+        </div>
+        <div id="recept-buttons">
+            <a href="/cashier">
+                <button class="btn btn-back">
+                    Back to Cashier
+                </button>
+            </a>
+           
+            <button class="btn btn-print" type="button" onclick="window.print(); return false;">
+                Print page
+            </button>
+        </div>
+    </div>
 </body>
 </html>
